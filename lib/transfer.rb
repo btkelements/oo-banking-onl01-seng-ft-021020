6a539af5d1cd3e8b220c1892144291e9b1f80262
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-06 18:22:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+  
+  def execute_transaction
+    if @sender.balance > @amount && status == "pending"
+  end
+  end  
+  
 end
