a78bb582448039667dbdf303118b6a48096afc79
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-06 19:18:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else @sender.balance < @amount && @status = "pending" && !valid?
-      status = "rejected"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
